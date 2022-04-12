ef2324554bf0a31c0977cc7992b22563acfff6a4
wxinix <xinwuping007@gmail.com>
2022-04-12 18:45:40 -0400

    Implemented getline_asyn for asynchronously loading file by multiple threads. Not tested yet.

--- mio/include/mio/stringreader.hpp
+++ mio/include/mio/stringreader.hpp
@@ -22,6 +22,10 @@
 
 #include <mio/mio.hpp>
 
+#include <functional>
+#include <future>
+#include <numeric>
+
 #ifdef __GNUC__
 #define semi_branch_expect(x, y) __builtin_expect(x, y)
 #else
@@ -41,16 +45,19 @@ namespace mio {
      mio::StringReader reader(file_path.string());
 
      if (reader.is_mapped()) {
-       auto line = reader.getline();
        while (!reader.eof()) {
-         counter++;
-         line = reader.getline();
+         auto line = reader.getline();
+         // ... do something about the line just read.
        }
      }
  */
 class StringReader
 {
 public:
+  /**
+    Fires when a new line has been read.
+  */
+  using OnGetline = std::function<void(const std::string_view)>;
 
   /**
      Constructs a reader to read from a disk file line by line. If the
@@ -123,6 +130,86 @@ public:
       m_begin = (l_begin = nullptr), (l_find = nullptr); // Set BOTH l_begin AND l_find nullptr if end of file.
 
     return {l_begin, static_cast<size_t>(l_find - l_begin)};
+  }
+
+  size_t getline(const OnGetline &a_on_getline)
+  {
+    size_t l_numlines{0};
+
+    // Fall back to synchronous line by line processing.
+    while (!this->eof()) {
+      a_on_getline(this->getline());
+      l_numlines++;
+    }
+    return l_numlines;
+  }
+
+  /**
+   Reads a new line asynchronously.
+   Precondition - StringReader::is_mapped() must be true.
+
+   \param a_on_getline Event handler when a new line is read.
+   \tparam NumThreads Number of threads to run parallel.
+   \tparam MinFileByteSize Minimum byte size of the file required for async processing to kick in.
+
+   \returns Total number of lines read.
+ */
+  template<uint8_t NumThreads = 2, size_t MinFileByteSize = 1024 * 1024>
+  requires (NumThreads >= 2) and (NumThreads <= 8) and (MinFileByteSize >= 1024 * 1024)
+  [[maybe_unused]] size_t getline_async(const OnGetline &a_on_getline)
+  {
+    // Fallback to default sequential getline.
+    if (m_mmap.size() < MinFileByteSize)
+      return getline(a_on_getline);
+
+    // Spawn a couple of futures, and do async processing.
+    std::array<std::unique_ptr<std::future<size_t>>, NumThreads> l_futures;
+
+    const auto l_adv = m_mmap.size() / NumThreads;
+    const char *l_begin = m_begin;
+    const char *l_end = find_end(l_begin, l_adv);
+
+    for (int i = 0; i < l_futures.size(); i++) {
+      *l_futures[i] = std::async(do_getline_async, l_begin, l_end, a_on_getline);
+
+      l_begin = l_end;
+      l_end = find_end(l_begin, l_adv);
+
+      if (std::distance(l_end, m_mmap.end()) < l_adv)
+        l_end = m_mmap.end();
+    }
+
+    // Collect the total number of lines read.
+    return std::accumulate(l_futures.begin(), l_futures.end(), 0, [](auto &a, size_t b) { return a->get() + b; });
+  }
+
+private:
+  static size_t do_getline_async(const char *a_begin, const char *a_end, const OnGetline &a_on_getline)
+  {
+    const char *l_begin = a_begin;
+    const char *l_find = std::find(l_begin, a_end, '\n');
+
+    size_t l_counter{0};
+
+    while (l_find != a_end) {
+      a_on_getline({l_begin, static_cast<size_t>(l_find - l_begin)}), l_counter++;
+      l_begin = std::next(l_find);
+      l_find = std::find(l_begin, a_end, '\n');
+    }
+
+    return l_counter;
+  }
+
+  static const char *find_end(const char *a_begin, size_t a_advance)
+  {
+    using iter_diff_t = typename std::iterator_traits<const char *>::difference_type;
+
+    auto l_find = std::find(
+        std::reverse_iterator(std::next(a_begin, static_cast<iter_diff_t>(a_advance))),
+        std::reverse_iterator(a_begin),
+        '\n').base();
+
+    return std::next(l_find);
   }
 
 private:
